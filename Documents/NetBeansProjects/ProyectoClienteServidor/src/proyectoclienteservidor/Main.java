d81416d48cb1719237f8fe50cf13e28f38edcd59
angevv <69274747+angevv@users.noreply.github.com>
2022-10-13 11:30:19 -0600

    Prueba

--- Documents/NetBeansProjects/ProyectoClienteServidor/src/proyectoclienteservidor/Main.java
+++ Documents/NetBeansProjects/ProyectoClienteServidor/src/proyectoclienteservidor/Main.java
@@ -4,17 +4,11 @@
  */
 package proyectoclienteservidor;
 
-/**
- *
- * @author Angel
- */
+
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
